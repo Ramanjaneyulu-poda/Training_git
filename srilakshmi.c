ba38299195105faf73cb19a60aef7a3c7bc6d6c6
lingala12 <srilakshmi.lingala123@gmail.com>
2023-08-22 16:18:59 +0530

    file added by lakshmi

---
+++ srilakshmi.c
@@ -0,0 +1,21 @@
+// ********* find largest and smallest in an array ********
+#include<stdio.h>
+int main()
+{
+	int arr[10],i,small,large,n;
+	printf("enter the sizeof array\n");
+	scanf("%d",&n);
+	printf("enter the array elements\n");
+	for(i=0;i<n;i++)
+		scanf("%d",&arr[i]);
+	small=arr[0];
+	large=arr[0];
+	for(i=0;i<n;i++)
+	{
+		if(arr[i]>large)
+			large=arr[i];
+		if(arr[i]<small)
+			small=arr[i];
+	}
+	printf("%d is large\n %d is small\n",large,small);
+}
