929fb0d62255008f4d481e89781175f181c2b8b4
chandramurali1 <cchandramurali@gmail.com>
2023-08-23 14:32:37 +0530

    driver file

--- driver.c
+++ driver.c
@@ -1,35 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 #include <stdio.h>
-=======
-<<<<<<< HEAD
-by anburaj
-vishnu
-=======
-hi this saravanan
->>>>>>> 65fff64dff89dac0bc110924b7b9d604d29e9b3f
->>>>>>> e4c5e2496401fe1cf1474efcd1cae55c10bf66f7
-=======
-vishnu 2nd change
-#include<stdio.h>
->>>>>>> cdec4cbba7ebc7e71d37f7116fdc5c37cb446e7e
 int main()
 {
-printf("driver.c file");
+printf("murali's driver file");
 }
 
-<<<<<<< HEAD
-=======
-
-#include<stdio.h>
-int main()
-{
-	printf(" driver file");
-	return 0;
-
-}
-
-hi this vishnu
-
->>>>>>> 6a280e4c102255e4cf628bea8224dd1082247598
->>>>>>> e4c5e2496401fe1cf1474efcd1cae55c10bf66f7
